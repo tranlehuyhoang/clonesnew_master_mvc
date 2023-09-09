93db9e1380efea85654b76c551f7e656fe8e6cb0
Huyh Huyhu <2509roblox@gmail.com>
2023-09-09 10:44:11 +0700

    5

--- app/views/frontend/orders_detail.php
+++ app/views/frontend/orders_detail.php
@@ -15,7 +15,7 @@ $fm = new Format();
         <div class="modal-dialog modal-xl" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    -->
+
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
